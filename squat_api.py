c794b04db1238e22d00bec682b2d4786f6158c03
Yuan Chen <67477126+andy1213812@users.noreply.github.com>
2024-02-28 15:45:48 -0500

    update api test file

--- squat_api.py
+++ squat_api.py
@@ -1,27 +1,21 @@
 from flask import Flask, request, jsonify
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
+
 model = load_model('LSTM.h5')
 
-data_path = 'Clean_Dataset/Correct_Squat_1.xlsx'
-
-
-@app.route('/predict', methods=['POST','GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
-        json_ = data_path
-        query_df = pd.DataFrame(json_)
-        scaler = StandardScaler()
-        query = scaler.fit_transform(query_df)
-        prediction = model.predict(query)
-        return jsonify({'prediction': prediction.flatten().tolist()})
+        data = request.get_json(force=True)
+        prediction_data = np.array(data['data'])
+        prediction = model.predict(prediction_data).tolist()
+        return jsonify({'prediction': prediction})
     except Exception as e:
         return jsonify({'error': str(e)})
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
